5a323eeff868dffb00435a1430a1c20829fe7ebb
“[Vertrowd]” <[rudraps9023@yahoo.com]>
2025-04-13 09:43:15 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/Elites_Hackverse/tree/main",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
