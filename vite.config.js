c0c255dd309008cacafae62eff4be945658e7734
vanshika07 <vanshikabhadani07@gmail.com>
2025-04-13 10:17:58 +0530

    temp

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
@@ -6,21 +5,13 @@ import { dirname, resolve } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
->>>>>>> 7a88e9cd95f526755146bf4b3558b2b8f230432a
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': resolve(__dirname, 'src'), // Alias '@' to the 'src' directory
     },
   },
 });
-=======
-})
->>>>>>> 7a88e9cd95f526755146bf4b3558b2b8f230432a
